598460d151700c7159e398893e38cd66989593ae
V3Solution <mubeenyasin75@gmail.com>
2023-12-30 14:11:13 +0500

    code commited by mubeen

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import {ApiError} from "../utils/ApiError.js"
+// import { User} from "../models/user.model.js"
+// import {uploadOnCloudinary} from "../utils/cloudinary.js"
+// import { ApiResponse } from "../utils/ApiResponse.js";
+// import jwt from "jsonwebtoken"
+// import mongoose from "mongoose";
+
+//Register Controller
+const registerUser = async('/', (req, res) => {
+
+   res.send('hello from simple server :)')
+
+})
+
+
+
+
+export {
+    registerUser
+}
