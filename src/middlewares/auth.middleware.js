2e7b435069b4ac0b5e7cbec8af9ea53699cf085b
V3Solution <mubeenyasin75@gmail.com>
2024-01-08 16:16:37 +0500

    working on the auth middleware 02

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,12 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-
+export const verifyJwt = asyncHandler(async (req, _, res) => {
+    try {
+        
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+});
