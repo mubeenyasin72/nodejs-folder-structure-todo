8d5a0b4067ede600116b0bf7aa2a2a372e0be91d
V3Solution <mubeenyasin75@gmail.com>
2024-01-19 13:28:37 +0500

    working on todo controller

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -5,6 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 
 const createTodo = asyncHandler(async (req, res, next) => {
+    const { taskName, isCompleted } = req.body;
     
 })
 
