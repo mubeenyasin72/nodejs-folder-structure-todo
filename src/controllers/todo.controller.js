9f9f2a17d630c9303eef856d09b63feafbbe3fe2
V3Solution <mubeenyasin75@gmail.com>
2024-01-20 13:54:24 +0500

    working start on task controller

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -2,10 +2,19 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import {TodoTask} from "../models/todo.model.js"
 import mongoose from "mongoose";
 
 const createTodo = asyncHandler(async (req, res, next) => {
     const { taskName, isCompleted } = req.body;
+
+    const todoTask = await TodoTask.create({
+        taskName,
+        isCompleted
+    })
+    return res
+    .status(201)
+    .json(new ApiResponse(200, todoTask, "Task Created Successfully"));
     
 })
 
