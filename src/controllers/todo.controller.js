12c95e5ae6b6788cbb4ae3615017b71b8454fcd9
V3Solution <mubeenyasin75@gmail.com>
2024-03-01 15:01:57 +0500

    get single todo task13

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -27,7 +27,6 @@ const getAllTodoTask = asyncHandler(async(req, res, next) => {
 const updateTodoController = asyncHandler(async(req, res, next) => {
     
 })
-//Get Single Todo Controller
 const getSingleTodoTask = asyncHandler(async (req, res, next) => {
     
 })
